9087df696bca86bc84b0821a09ce5eeebc36e640
akshaykrishnam80 <88880305+akshaykrishnam80@users.noreply.github.com>
2023-11-11 09:43:44 +0000

    enable disable btn cart, zero cart issue solve

---
+++ js/items.js
@@ -0,0 +1,34 @@
+const Items =[
+    { 
+        "id": 1,
+        "item_name": "Language Arts",
+        "item_image": "images/no-itemimage.png",
+        "price": 79,
+        "qty": 7,
+        "location": "London",
+    },
+    { 
+        "id": 2,
+        "item_name": "Advanced Calculus",
+        "item_image": "images/no-itemimage.png",
+        "price": 109,
+        "qty": 10,
+        "location": "Reading",
+    },
+    { 
+        "id": 3,
+        "item_name": "English",
+        "item_image": "images/no-itemimage.png",
+        "price": 39,
+        "qty": 5,
+        "location": "London",
+    },
+    { 
+        "id": 4,
+        "item_name": "Mathematics Level 5",
+        "item_image": "images/no-itemimage.png",
+        "price": 39,
+        "qty": 5,
+        "location": "London",
+    }
+]
